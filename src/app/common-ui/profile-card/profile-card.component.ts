3a2592f9ba485c1b5a700d953db9f189f956df9c
Kuldyaev <slavalion1@gmail.com>
2025-03-29 01:40:58 +0200

    start use pipeline'

--- src/app/common-ui/profile-card/profile-card.component.ts
+++ src/app/common-ui/profile-card/profile-card.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Profile } from '../../data/interfaces/profile.interface';
+import { ImgUrlPipe } from '../../helpers/pipes/img-url.pipe';
+import { UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-profile-card',
-  imports: [],
+  imports: [ImgUrlPipe, UpperCasePipe],
   templateUrl: './profile-card.component.html',
   styleUrl: './profile-card.component.scss',
 })
