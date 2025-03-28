c0e294f09fb8d54931d5b6bde443dbd8701814e1
Kuldyaev <slavalion1@gmail.com>
2025-03-29 00:44:12 +0200

    tS interface to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+// import { CommonModule } from '@angular/common';
 import { ProfileCardComponent } from './common-ui/profile-card/profile-card.component';
+import { ProfileService } from './data/services/profile.service';
+import { Profile } from './data/interfaces/profile.interface';
 
 @Component({
   selector: 'app-root',
-  imports: [/*RouterOutlet, */ ProfileCardComponent],
+  imports: [/*RouterOutlet, */ ProfileCardComponent /*, CommonModule*/],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  profileService = inject(ProfileService);
+  profiles: Profile[] = [];
   title = 'tik-talk';
+
+  constructor() {
+    this.profileService
+      .getTestAccounts()
+      .subscribe((val: Profile[]) => (this.profiles = val));
+  }
 }
